3e9ce11e0196a6d2c2067cf73ea65f55ea146b33
Heidi <hdpeterson75@gmail.com>
2019-09-23 11:10:20 -0400

    update

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 
+#Note file explains how I came to this way to make the datafile
 getwd()
 File<- file("household_power_consumption.txt")
 Data <- read.table(text = grep("^[1,2]/2/2007", readLines(File), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", na.strings = "?", header = TRUE, stringsAsFactors=FALSE )
@@ -10,8 +11,13 @@ Data$Datetime <- as.POSIXct(datetime)
 
 str(Data)
 
+?hist()
+
+#generic function R hist 
+
 hist(Data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
 
+#ggplot2 function hist
 ggplot(Data, aes(x = Global_active_power)) + 
   geom_histogram(fill = "red")+
   ggtitle("Global Active Power")+
