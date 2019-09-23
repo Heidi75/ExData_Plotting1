c779296e06c361d866cdb32a0b0c2f87654c3972
Heidi <hdpeterson75@gmail.com>
2019-09-23 11:03:32 -0400

    Plot1
    testing push git

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(ggplot2)
 
-getwd()  ### See the notes to how i decided to load this dataframe
+getwd()
 File<- file("household_power_consumption.txt")
 Data <- read.table(text = grep("^[1,2]/2/2007", readLines(File), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", na.strings = "?", header = TRUE, stringsAsFactors=FALSE )
 
@@ -12,11 +12,11 @@ str(Data)
 
 hist(Data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
 
-
-#####   THIS CODE IS MAKING SAME HISTOGRAM USING GGPLOT2
-
 ggplot(Data, aes(x = Global_active_power)) + 
   geom_histogram(fill = "red")+
   ggtitle("Global Active Power")+
   xlab("Global Active Power (kilowatts)")+
   ylab("Frequency")
+
+
+
