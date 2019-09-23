9e0ed7a41f987f0ae8c8b5424c29c334f2e942c2
Heidi <54006359+Heidi75@users.noreply.github.com>
2019-09-22 22:57:11 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(ggplot2)
+
+getwd()  ### See the notes to how i decided to load this dataframe
+File<- file("household_power_consumption.txt")
+Data <- read.table(text = grep("^[1,2]/2/2007", readLines(File), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", na.strings = "?", header = TRUE, stringsAsFactors=FALSE )
+
+datetime <- paste(as.Date(Data$Date), Data$Time)
+Data$Datetime <- as.POSIXct(datetime)
+
+str(Data)
+
+hist(Data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+
+
+#####   THIS CODE IS MAKING SAME HISTOGRAM USING GGPLOT2
+
+ggplot(Data, aes(x = Global_active_power)) + 
+  geom_histogram(fill = "red")+
+  ggtitle("Global Active Power")+
+  xlab("Global Active Power (kilowatts)")+
+  ylab("Frequency")
